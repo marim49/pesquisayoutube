a56bacee786b56bcc04e1cb6caba43a7f50957b3
Leandro Marim <leandro.marim@hotmail.com>
2018-02-25 20:13:45 -0300

    Implementado busca e retornando resultados
    retornando resultados com JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpModule, Http } from '@angular/http';
+ 
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  pesquisa: string;
+  lista_videos: any = new Array <any> ();
+  url = "https://www.googleapis.com/youtube/v3/";
+  API_KEY = "AIzaSyDL_9RACZ1ob7ocA4C-EDOf-kareRz9eBE";
+pesquisar(){
+  return this.busca.get(this.url+"search?q="+this.pesquisa+"&key="+this.API_KEY+ "&part=snippet")
+  .toPromise()
+  .then(resposta=>{
+    this.lista_videos = resposta.json().items;
+  })
+  
+  
+          }
+
+teste (){
+  console.log(this.pesquisar())
+  
+}
+
+constructor(public busca: Http){
+ this.teste();
+  
+
+}
+
+
   title = 'app';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http'
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+
+
 
 
 
@@ -14,7 +17,14 @@ import { NavbarComponent } from './navbar/navbar.component';
     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    
+    
+    
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
