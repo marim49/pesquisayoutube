b4c785b30087c4d117b031aa52f7808e118433ed
Leandro Marim <leandro.marim@hotmail.com>
2018-02-26 19:08:55 -0300

    listando videos no html (não concluído)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,30 +19,36 @@ export class AppComponent {
   lista_videos: any = new Array <any> ();
   url = "https://www.googleapis.com/youtube/v3/";
   API_KEY = "AIzaSyDL_9RACZ1ob7ocA4C-EDOf-kareRz9eBE";
-  youtube = "http://www.youtube.com/embed/";
+  youtube = 'http://www.youtube.com/embed/';
+  videourl: any;
   
+
 
 pesquisar(){
   return this.busca.get(this.url+"search?q="+this.pesquisa+"&key="+this.API_KEY+ "&part=snippet")
   .toPromise()
   .then(resposta=>{
     this.lista_videos = resposta.json().items; 
+    /* this.videourl = this.dom.bypassSecurityTrustResourceUrl(this.youtube +  this.lista_videos.id.videoId);
+    */
+
      
    })
 
   
-  
+   
           }
+ 
 
 teste (){
   console.log(this.pesquisar())
   
 }
 
-constructor(public busca: Http, private dom: DomSanitizer){
+constructor(public busca: Http,private dom: DomSanitizer){
   
  this.teste();
-  
+
 
 }
 
