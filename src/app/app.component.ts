25888a3c058073ae619d895b59f5d12c419b8583
Leandro Marim <leandro.marim@hotmail.com>
2018-02-25 23:40:01 -0300

    Busca de videos por ID
    Tentando achar uma solução para busca de canais por id, pois interpolando na src do iframe da erro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,17 @@ import { HttpModule, Http } from '@angular/http';
 })
 export class AppComponent {
 
-  pesquisa: string = "inutilismo";
+  pesquisa: string;
   lista_videos: any = new Array <any> ();
   url = "https://www.googleapis.com/youtube/v3/";
   API_KEY = "AIzaSyDL_9RACZ1ob7ocA4C-EDOf-kareRz9eBE";
+  youtube = "http://www.youtube.com/embed/";
 pesquisar(){
   return this.busca.get(this.url+"search?q="+this.pesquisa+"&key="+this.API_KEY+ "&part=snippet")
   .toPromise()
   .then(resposta=>{
     this.lista_videos = resposta.json().items;
+    
   })
   
   
